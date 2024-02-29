95442863b7b457b2a2b89db270e83b588a1d1c72
ganeshkanade04 <157108655+ganeshkanade04@users.noreply.github.com>
2024-02-29 20:42:14 +0000

    Initail Setup

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from './Data';
+
+function App() {
+  return (
+    <div className="App">
+      <Data />
+      <h1> Ganesh Kanade</h1>
+    
+      <Data />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Data.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+// function Data(){
+//     return(
+//         <div>
+//             <h1> 23-09-2003</h1>
+//             <h2>I will become best version of myself</h2>
+//         </div>
+//     )
+// }
+
+// export default Data;
